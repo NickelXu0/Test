16c537086788eb974b199ed1b1bfea8c281c0cbe
NickelXu0 <89847519+NickelXu0@users.noreply.github.com>
2023-05-12 23:51:52 -0400

    Add files via upload

---
+++ response.js
@@ -0,0 +1,9 @@
+
+const responseObj = {
+  hello: "Hey ! How are you doing ?",
+  hey: "Hey! What's Up",
+  hi: "Hey! What's Up",
+  howdy:"Hey ! How are you doing ?",
+  today: new Date().toDateString(),
+  time: new Date().toLocaleTimeString(),
+};
