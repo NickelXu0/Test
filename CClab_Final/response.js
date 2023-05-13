1300caf8201204168fdb4c2bd50655613cb031dd
NickelXu0 <89847519+NickelXu0@users.noreply.github.com>
2023-05-12 23:50:42 -0400

    Delete CClab_Final directory

--- CClab_Final/response.js
+++
@@ -1,9 +0,0 @@
-
-const responseObj = {
-  hello: "Hey ! How are you doing ?",
-  hey: "Hey! What's Up",
-  hi: "Hey! What's Up",
-  howdy:"Hey ! How are you doing ?",
-  today: new Date().toDateString(),
-  time: new Date().toLocaleTimeString(),
-};
